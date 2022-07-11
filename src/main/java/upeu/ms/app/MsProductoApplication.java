6cc59ce1ca8794127834dc1400dce6f52cc6cf04
joseht8 <joseht8@gmail.com>
2022-07-11 13:43:34 -0500

    first commit

---
+++ src/main/java/upeu/ms/app/MsProductoApplication.java
@@ -0,0 +1,15 @@
+package upeu.ms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MsProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/upeu/ms/app/entity/Producto.java
@@ -0,0 +1,39 @@
+package upeu.ms.app.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "mae_producto")
+public class Producto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private Double precio;
+	@Column(name = "create_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createAt;
+	
+	@Column(name = "estado")
+	private Boolean estado;
+	
+	@Transient
+	private Integer port;
+}
